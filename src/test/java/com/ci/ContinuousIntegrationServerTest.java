3728f7002254634aa10728a1b3ca197e6736b9dc
Samuel Falk <71826609+FalkWasTaken@users.noreply.github.com>
2023-02-09 09:48:38 +0100

    Update ContinuousIntegrationServerTest.java

--- src/test/java/com/ci/ContinuousIntegrationServerTest.java
+++ src/test/java/com/ci/ContinuousIntegrationServerTest.java
@@ -16,7 +16,7 @@ public class ContinuousIntegrationServerTest {
         @Test
         public void verifyNoExceptionsThrown(){
             DEFAULT.gradleTest();
-            assertTrue(false)
+            assertTrue(false);
         }
 
         @Test
